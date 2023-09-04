6cb28ed82e8577de2f96d900a4339c2e957d713b
srinivasaoo1 <srinivasa9.marolix@gmail.com>
2023-09-04 15:48:28 +0530

    thursday work

--- assign/arrange.py
+++ assign/arrange.py
@@ -1,14 +1,1 @@
-def get_distinct_elements(input_list):
-    unique_elements = []
-    seen = set()
 
-    for item in input_list:
-        if item not in seen:
-            unique_elements.append(item)
-            seen.add(item)
-
-    return unique_elements
-
-input_list = eval(input('the the list:'))
-distinct_elements = get_distinct_elements(input_list)
-print(distinct_elements)
